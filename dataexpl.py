edf9ea2ada9a8752097c2083deab47c04925f19e
Andrea Donati <donati.andrea.00@gmail.com>
2017-04-20 10:59:39 +0200

    aggiornato il codice di bomber

--- dataexpl.py
+++ dataexpl.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -- coding: utf-8 --
 """
 Created on Thu Apr 13 18:15:03 2017
 
@@ -29,16 +29,17 @@ for i in range(0,len(cellnames)):
    tot_activity_per_cell.append([cellnames[i], sum(df[df['squareid']==cellnames[i]].activity),lat,lon ])
 
 df2 = pd.DataFrame(tot_activity_per_cell)
-df2.columns=['id','activity']
+df2.columns=['id','activity','lat','lon']
 df2= df2.sort_values('activity', ascending=False)
 
 #Qui plotto la mappa
 gmap = gmplot.GoogleMapPlotter(45.4822916634, 9.1875001735, 13)
 lat= [x[2] for x in tot_activity_per_cell]
 lon= [x[3] for x in tot_activity_per_cell]
-gmap.plot(lat, lon, 'cornflowerblue', edge_width=2)
+
+#gmap.plot(lat, lon, 'cornflowerblue', edge_width=2)
 #gmap.scatter([45.482291], [9.187500], '#3B0B39', size=40, marker=False)
 #gmap.scatter(marker_lats, marker_lngs, 'k', marker=True)
-gmap.heatmap([45.482291], [9.187500])
+gmap.heatmap(lat, lon)
 
 gmap.draw("mymap.html")
