67da7be0d2c800de3049958983ade8c238155d5d
Andrea Donati <donati.andrea.00@gmail.com>
2017-04-22 16:06:57 +0200

    Aggiunti i dati dell'app

--- app_data_from_bson_to_json.py
+++ app_data_from_bson_to_json.py
@@ -1,15 +1,16 @@
 import bson
 import json
 
-with open('Data\\fuorisalone_2016_anonymous_appdata\\event.bson', 'rb') as f:
+with open('Data/fuorisalone_2016_anonymous_appdata/anon_db/event.bson', 'rb') as f:
     coll_raw = f.read()
 
 a = bson.loads(coll_raw)
 type(a)
 print(a)
 
-json_array = json.dumps(a)
 del a['_id']
+json_array = json.dumps(a)
+type(json_array)
 
-out = open('Data\\fuorisalone_2016_anonymous_appdata\\event.json', 'w')
+out = open('Data/fuorisalone_2016_anonymous_appdata/anon_db/event.json', 'w')
 out.writelines(json_array)
