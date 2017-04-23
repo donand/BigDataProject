c903593c9eca93bc59aede01d9ea711603257225
RiccardoDeTogni <riccardo.detogni@mail.polimi.it>
2017-04-23 16:04:27 +0200

    DUMPED AF YESSSSS

--- dataexp.py
+++ dataexp.py
@@ -8,6 +8,7 @@ Created on Fri Apr 14 11:37:10 2017
 import pandas as pd
 import csv
 import numpy as np
+
 dataset = pd.read_csv('Data/fuorisalone_foursquare-history-2016-04.csv', converters={'history.date': lambda x: x.replace('T',''), 'history.date': lambda x: x.replace('Z','')})
 dataset['history.date']=pd.to_datetime(dataset['history.date'],format='%Y-%m-%dT%H:%M')
 
