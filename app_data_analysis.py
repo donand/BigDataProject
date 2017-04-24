7c0ca7c702d6b7838f491428ec8abce8c1202c8c
RiccardoDeTogni <riccardo.detogni@mail.polimi.it>
2017-04-24 12:20:38 +0200

    Some plots

--- app_data_analysis.py
+++ app_data_analysis.py
@@ -10,29 +10,40 @@ import json
 from pandas.io.json import json_normalize
 import gmplot
 import gmaps
+import numpy as np
 
 df = pd.read_json("C:\Users\Riccardo\Documents\GitHub\BigDataProject\Data\fuorisalone_2016_anonymous_appdata\anon_db\event.json")
 event_list = []
 with open('Data/fuorisalone_2016_anonymous_appdata/anon_db/events_analytic.json') as f:
     text = f.read()
     for line in text.split('\n'):
-        event_list.append(json.loads(line))
+        try:
+            event_list.append(json.loads(line))
+        except:
+            pass
 df_event = json_normalize(event_list)
 
 position_list = []
 with open('Data/fuorisalone_2016_anonymous_appdata/anon_db/position.json') as f:
     text = f.read()
     for line in text.split('\n'):
-        position_list.append(json.loads(line))
+        try:
+            position_list.append(json.loads(line))
+        except:
+            pass
 df_position = json_normalize(position_list)
+del position_list
+del text
 
-lats = df_position['latitude']
-longs = df_position['longitude']
-
+lats = df_position[df_position.latitude > 45.428129]
+lats = lats[lats.latitude < 45.497044]
+longs = lats['longitude']
+lats = lats['latitude']
+test = df_event.to_dict()
 
 gmap = gmplot.GoogleMapPlotter(45.4822916634, 9.1875001735, 13)
-#gmap.heatmap(lats[:], longs[:], radius = 50, threshold = 1, opacity = 0.5, dissipating= True)
-gmap.scatter(lats[:200],longs[:200], '#3B0B39', size=40, marker=False)
+gmap.heatmap(lats[:], longs[:], radius = 50, threshold = 1, opacity = 0.5, dissipating= True)
+#gmap.scatter(lats[:1000],longs[:1000], '#3B0B39', size=40, marker=False)
 gmap.draw("position.html")
 
 
@@ -58,7 +69,7 @@ m.add_layer(gmaps.heatmap_layer([(lat, lng) for lat,lng in zip(lats[:200],longs[
 tmp['date']=pd.to_datetime(df_event['date'], unit = 'ms').normalize()
 
 df_event['event'].value_counts()
-
+    
 aggr = df_event.groupby([pd.DatetimeIndex(df_event['date']).normalize(),'event'])
 
 df_event.index = df_event['date']
