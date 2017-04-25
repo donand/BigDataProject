6eec3f8bd8a350ad91f0ced80257f1426bce39f5
RiccardoDeTogni <riccardo.detogni@mail.polimi.it>
2017-04-25 18:25:58 +0200

    BAH

--- DataExploration/app_data_position.py
+++ DataExploration/app_data_position.py
@@ -19,17 +19,20 @@ import matplotlib.pyplot as plt
 DEG_DIST_LAT = 111142
 DEG_DIST_LON = 78100
 
-timeslots = {0 : {start : 0, end : 6}, 1 : {start : 6, end : 12}, 2 : {start : 12, end : 18}, 3 : {start : 18, end : 24}}
+timeslots = {0 : {'start' : 0, 'end' : 6}, 1 : {'start' : 6, 'end' : 12}, 2 : {'start' : 12, 'end' : 18}, 3 : {'start' : 18, 'end' : 24}}
 
 df_position = pd.read_csv("Data/fuorisalone_2016_anonymous_appdata/anon_db/position.csv", encoding = 'latin1')
-event = pd.read_csv("", encoding = 'latin1')
+
+df_position['date']=pd.to_datetime(df_position['date'])
+df_position = df_position[(df_position['date'] > pd.to_datetime('11-04-2016',format='%d-%m-%Y')) &\
+                          (df_position['date'] <= pd.to_datetime('18-04-2016',format='%d-%m-%Y'))]
+
+event = pd.read_csv("Data/fuorisalone_2016_anonymous_appdata/anon_db/events_with_timeslots.csv", encoding = 'latin1')
 
 def count_pos(timeslot, latitude, longitude):
     mask = (np.sqrt(((df_position['latitude'].map(lambda x: x - latitude)) * DEG_DIST_LAT)**2 + ((df_position['longitude'].map(lambda x: x - longitude)) * DEG_DIST_LON)**2) < 150) &\
-    (df_position['date'] > pd.to_datetime('11-04-2016',format='%d-%m-%Y')) &\
-    (df_position['date'] <= pd.to_datetime('18-04-2016',format='%d-%m-%Y')) &\
-    (df_position.date.dt.hour < timeslots[timeslot][end]) &\
-    (df_position.date.dt.hour >= timeslots[timeslot][start])
+    (df_position.date.dt.hour < timeslots[timeslot]['end']) &\
+    (df_position.date.dt.hour >= timeslots[timeslot]['start'])
     return len(df_position[mask])
 
 event['position_count'] = pd.Series([count_pos(row['timeslot'],row['latitude'],row['longitude']) for index, row in event.iterrows()])
@@ -40,6 +43,6 @@ cells = phone.drop_duplicates('squareid')
 cells = cells[['squareid','lat','lon']].reset_index(drop = True)
 
 def belong_to_cell(lat_event, lon_event, lat_cell, lon_cell):
-    return abs(lat_event - lat_cell) * DEG_DIST_LAT < 75 & abs(lon_cell - lon_event) * DEG_DIST_LON < 75
+    return (abs(lat_event - lat_cell) * DEG_DIST_LAT < 75.0) & (abs(lon_cell - lon_event) * DEG_DIST_LON < 75.0)
 
-event['phone_count'] = pd.Series([cell['activity'] for index, row in event.iterrows() for index, cell in cells.iterrows() if belong_to_cell(row['latitude'], row['longitude'], cell['lat'], cell['lon'])])
+event['phone_count'] = pd.Series([cell['activity'] for index, row in event.iterrows() for index, cell in phone.iterrows() if belong_to_cell(row['latitude'], row['longitude'], cell['lat'], cell['lon'])])
