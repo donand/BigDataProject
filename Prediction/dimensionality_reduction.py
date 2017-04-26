7d275bc446a1f55a4006c95edf2917ca4318b6f7
Andrea Donati <donati.andrea.00@gmail.com>
2017-04-26 18:20:31 +0200

    Iniziato a guardare dimensionality reduction e encodato i cluster

---
+++ Prediction/dimensionality_reduction.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import OneHotEncoder
+
+df = pd.read_csv('Prediction/events_ready_to_pre.csv', encoding = 'latin1')
+
+df.cluster = df.cluster.map(lambda x: x - min(df.cluster))
+
+one_hot_enc = OneHotEncoder()
+a = one_hot_enc.fit_transform(df.cluster.reshape(-1, 1)).toarray()
+
+a = pd.DataFrame(a)
+df = pd.concat([df, a], axis = 1)
+
+# drop cluster 0, the outliers of the outliers
+df.drop(0, axis = 1, inplace = True)
+df.drop('cluster', axis = 1, inplace = True)
+
